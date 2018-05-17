4bdc534eff3a37958786599277ef8ba2eb4d3c66
aoki-yu <aoki-yu@gnavi.co.jp>
2018-05-17 17:25:58 +0900

    change

--- posenet_sample.js
+++ posenet_sample.js
@@ -12,6 +12,13 @@
 * いかなる保証も条件もなしに「現状のまま」頒布されます。
 * 本ライセンスでの権利と制限を規定した文言については、本ライセンスを参照してください。
 */
+/*
+* このプログラムは
+* https://github.com/tensorflow/tfjs-models/tree/master/posenet/demos
+* および
+* https://github.com/tensorflow/tfjs-models/blob/master/posenet/demos/camera.js
+* をもとに作成しました。
+*/
 
 const imageScaleFactor = 0.2;
 const outputStride = 16;
